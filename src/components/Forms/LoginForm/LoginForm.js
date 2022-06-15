76782beafe1a940ceb93e895fc50f8444308b897
LukasP <lukas.straubing@gmail.com>
2022-06-15 12:37:13 +0200

    login logic added

--- src/components/Forms/LoginForm/LoginForm.js
+++ src/components/Forms/LoginForm/LoginForm.js
@@ -1,12 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
 import styles from './LoginForm.module.css';
-import { authSliceActions, sendLoginData } from '../../../store/authSlice';
+import { sendLoginData } from '../../../store/authSlice';
+import { StartPage } from '../../StartPage/StartPage';
 
 export function LoginForm() {
-  const jwt = useSelector((state) => state.authSlice.jwt);
-  console.log(jwt);
   const dispatch = useDispatch();
+  const errorNotification = useSelector((state) => state.authSlice.notification);
+  const isLogged = useSelector((state) => state.authSlice.isLogged);
+  // I use local state for storing variables with email and password.
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const emailHandler = (e) => {
@@ -18,25 +20,32 @@ export function LoginForm() {
 
   const formSend = (e) => {
     e.preventDefault();
+    // I call thunk function to can send data to backend async
     dispatch(sendLoginData({ email, password }));
-    // dispatch(authSliceActions.loginUser({
-    //   email, password,
-    // }));
   };
-
   return (
-    <div className="centered">
-      <form className={styles.loginForm} onSubmit={formSend}>
-        <label htmlFor="loginEmail">
-          Email
-        </label>
-        <input type="email" id="loginEmail" onChange={emailHandler} required />
-        <label htmlFor="loginPassword">
-          Password
-        </label>
-        <input type="password" id="loginPassword" onChange={passwordHandler} required />
-        <button>Login</button>
-      </form>
-    </div>
+
+    !isLogged ? (
+      <>
+        {' '}
+        <div className="centered">
+          <form className={styles.loginForm} onSubmit={formSend}>
+            <label htmlFor="loginEmail">
+              Email
+            </label>
+            <input type="email" id="loginEmail" onChange={emailHandler} required />
+            <label htmlFor="loginPassword">
+              Password
+            </label>
+            <input type="password" id="loginPassword" onChange={passwordHandler} required />
+            <button>Login</button>
+          </form>
+        </div>
+        <div className="centered">
+          <p>{errorNotification}</p>
+        </div>
+      </>
+    ) : <StartPage />
+
   );
 }
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,38 +1,80 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialStateAuthSlice = {
-  email: '',
-  password: '',
+  user: {
+    email: '',
+    tasks: [],
+    lastLogin: '',
+    registerAt: '',
+    userID: '',
+  },
   jwt: '',
   isLogged: false,
+  notification: '',
+
 };
 const authSlice = createSlice({
   name: 'authSlice',
   initialState: initialStateAuthSlice,
   reducers: {
-    loginUser(state, action) {
-      state.email = action.payload.email;
-      state.password = action.payload.password;
+    setUserData(state, action) {
+      state.user.email = action.payload.email;
+      state.user.lastLogin = action.payload.lastLogin;
+      state.user.registerAt = action.payload.registerAt;
+      state.user.userID = action.payload.userID;
+      state.isLogged = true;
     },
     setJwt(state, action) {
       state.jwt = action.payload;
     },
+    setNotification(state, action) {
+      state.notification = action.payload;
+    },
   },
 });
 export const authSliceActions = authSlice.actions;
-export const sendLoginData = (data) => async (dispatch) => {
-  const response = await fetch('http://localhost:3001/v1/api/auth', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      email: data.email,
-      password: data.password,
-    }),
-  });
 
-  const jwt = await response.json();
-  dispatch(authSliceActions.setJwt(jwt));
+// thunk function to use async fetch
+export const sendLoginData = (data) => async (dispatch) => {
+  dispatch(authSliceActions.setNotification(''));
+  try {
+    const response = await fetch('http://localhost:3001/v1/api/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: data.email,
+        password: data.password,
+      }),
+    });
+    if (!response.ok) {
+      dispatch(authSliceActions.setNotification('Email or password incorrect'));
+      return;
+    }
+    const jwt = await response.json();
+    console.log(`jwt ${jwt}`);
+    dispatch(authSliceActions.setJwt(jwt));
+    const userData = await fetch('http://localhost:3001/v1/api/user', {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${jwt}`,
+      },
+    });
+    if (!userData.ok) {
+      dispatch(authSliceActions.setNotification('Ups...we have some error.'));
+      return;
+    }
+    const user = await userData.json();
+    console.log(`User data: ${user.email}`);
+    dispatch(authSliceActions.setUserData({
+      email: user.email,
+      userID: user.userID,
+      lastLogin: user.lastLogin,
+      registerAt: user.registerAt,
+    }));
+  } catch (err) {
+    console.log(err);
+  }
 };
 export default authSlice;
