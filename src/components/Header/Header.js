b7fcdfed0be4c25d423ad6cfd907a10db7215560
LukasP <lukas.straubing@gmail.com>
2022-06-15 13:49:10 +0200

    menu in header changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,39 @@ import { useSelector } from 'react-redux';
 import icon1 from '../../assets/icon1.png';
 import styles from './Header.module.css';
 
+const menuForLogged = (
+  <nav className={styles.nav}>
+    <ul>
+      <li>
+        <NavLink to="/aboutApp">About App</NavLink>
+      </li>
+      <li>
+        <NavLink to="/logout">Logout</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Main Page</NavLink>
+      </li>
+
+    </ul>
+  </nav>
+);
+const menuForNotLogged = (
+  <nav className={styles.nav}>
+    <ul>
+      <li>
+        <NavLink to="/aboutApp">About App</NavLink>
+      </li>
+      <li>
+        <NavLink to="/login">Login</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Main Page</NavLink>
+      </li>
+
+    </ul>
+  </nav>
+);
+
 export function Header() {
   const isLogged = useSelector((state) => state.authSlice.isLogged);
   return (
@@ -10,20 +43,7 @@ export function Header() {
       <div className={styles.title}>
         Tick it off
       </div>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <NavLink to="/aboutApp">About App</NavLink>
-          </li>
-          <li>
-            {!isLogged ? <NavLink to="/login">Login</NavLink> : 'Logout'}
-          </li>
-          <li>
-            <NavLink to="/">Main Page</NavLink>
-          </li>
-
-        </ul>
-      </nav>
+      {isLogged ? menuForLogged : menuForNotLogged}
       <div className={styles.icon}>
         <img src={icon1} alt="Task list icon" />
 
