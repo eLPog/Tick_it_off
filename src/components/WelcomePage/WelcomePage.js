25f0269d168b0876875f0dc68d95f315bbcefc96
LukasP <lukas.straubing@gmail.com>
2022-08-05 19:13:19 +0200

    navlinks to menu added

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -1,8 +1,5 @@
 import './WelcomePage.css';
-import aboutApp from '../../assets/images/photo-aboutApp.jpg';
-import homePage from '../../assets/images/photo-homePage.jpg';
-import login from '../../assets/images/photo-login.jpg';
-import register from '../../assets/images/photo-registration.jpg';
+import { NavLink } from 'react-router-dom';
 
 export function WelcomePage() {
   function removeActiveClass() {
@@ -20,25 +17,27 @@ export function WelcomePage() {
 
         <article className={`menuItem active bild-1`} onMouseEnter={addActiveClass}>
           <div className={`overlay`}>
-          <h3>Home</h3>
+              <NavLink to="/"><h3>Home</h3></NavLink>
           </div>
         </article>
         {' '}
       <article className={`menuItem bild-2`} onMouseEnter={addActiveClass}>
         <div className={`overlay`}>
-        <h3>Login</h3>
+            <NavLink to="/login"><h3>Login</h3></NavLink>
+
         </div>
       </article>
       {' '}
       <article className={`menuItem bild-3` } onMouseEnter={addActiveClass}>
         <div className={`overlay`}>
-        <h3>Sign In</h3>
+        <NavLink to="/register"><h3>Sign In</h3></NavLink>
         </div>
       </article>
       {' '}
       <article className={`menuItem bild-4`} onMouseEnter={addActiveClass}>
         <div className={`overlay`}>
-        <h3>About App</h3>
+            <NavLink to="/aboutApp"><h3>About App</h3></NavLink>
+
         </div>
       </article>
     </section>
